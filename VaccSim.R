9dd7258c8805a980cec0e3e7aaf9aaf67c461bb5
Steve Bellan <steve.bellan@gmail.com>
2015-01-28 02:33:26 -0600

    minor

--- VaccSim.R
+++ VaccSim.R
@@ -5,12 +5,13 @@ source('simFuns.R')
 
 yearToMonth <- 12/365.25
 meanHaz <- .2 * yearToMonth  ## converting from per year
-varC <- .1 * yearToMonth^2   ## cluster level
-varI <- .05 * yearToMonth^2  ## individual level
+varC <- meanHaz^2/2
+varI <- meanHaz^2/8
 
-breaks <- seq(0,.2, by = .01)
-hist(reParmRgamma(10^5, meanHaz, varC), col = 'black', xlab = 'hazard / month', main = 'distribution of cluster hazards', 
-     xlim = c(0, .1), breaks=breaks)
+samp <- reParmRgamma(10^5, meanHaz, varC)
+breaks <- 100
+hist(samp, col = 'black', xlab = 'hazard / month', main = 'distribution of cluster hazards', 
+     xlim = c(0, .05), breaks=breaks)
 title(main=paste('average hazard = ', signif(meanHaz,2)), line = -2)
 
 test <- makePop(20,300)
@@ -19,19 +20,10 @@ test <- setHazs(test, mean  = meanHaz, varClus = varC, varIndiv = varI)
 test <- simSWCT(test, vaccEff = .6)
 
 stest <- makeSurvDat(test)
-
 t1 <- truncSurvDat(stest, 6)
 
 summarise(group_by(t1, vacc), sum(infected))
 ## summarise(group_by(t1, vacc, cluster), sum(infected))
-
-mod <- coxph(Surv(startTime, endTime, infected) ~ vacc, data=t1)
-modF <- coxph(Surv(startTime, endTime, infected) ~ 
-             vacc + frailty.gamma(cluster, eps=1e-10, method="em", sparse=0),
-             outer.max=1000, iter.max=10000,
-             data=t1)
-summary(mod)
-summary(modF)
 
 hist(test[,infectTime], breaks = seq(0, max(test[,infectTime])+1, by = 1), xlim = c(0, 4), xlab = 'months')
 sum(test$infectTime < 30*4)
