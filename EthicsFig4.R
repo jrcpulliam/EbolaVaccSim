791935d0201796ff7e4624eca701788e53d8c3e3
Steven Bellan <sbellan@login2.ls5.tacc.utexas.edu>
2016-10-20 13:45:57 -0500

    minor, realized that contVaccDelay spends more cases with EV than without, need to figure out why and come up with a reasonable scenario where rolling vaccine out post-trial isn't worse than the however long delay

--- EthicsFig4.R
+++ EthicsFig4.R
@@ -16,12 +16,11 @@ figdir <- file.path('Figures', thing)
 dir.create(figdir)
 fls <- list.files('BigResults', pattern = paste0(thing,'-'), full.names=T)
 
-
 punq <- data.table()
 for(ii in 1:length(fls)) {
     load(fls[ii])
     tmp <- merge(resList$punq,resList$SpopWorst,by = c('pid', 'propInTrial','lab'))
-    tmp <- merge(tmp, resList$irsk[type=='marg', list(caseSpent = 6000*mean(spent_EV)), pid], by = 'pid')
+    tmp <- merge(tmp, resList$irsk[type=='marg', list(caseSpent_EV= 6000*mean(spent_EV), caseSpent= 6000*mean(spent)), pid], by = 'pid')
     speedpow <- with(resList, merge(parms[, list(pid, nbatch)], finTrials[,list(tcal, vaccEff,vaccGood, cvr,nbatch)], by = 'nbatch'))
     tmp <- merge(tmp, speedpow[, list(tcal = mean(tcal)), pid], by = 'pid')
     punq <- rbind(punq, tmp)
@@ -30,14 +29,14 @@ punq[,trialStartDate:=as.Date(trialStartDate)]
 punq[,date:=format.Date(trialStartDate, '%b-%y')]
 
 cols <- c("NT"='red', "SWCT"='orange', "VR"='dark green', "RCT-gs"='dodger blue', "RCT"='light blue', "RCT-rp"='purple', "RCT-gs-rp" = 'purple', 'RCT-gs-rp-cvd'='purple', 'RCT-gs-rp-maxRR'='purple')
-punq$cols <- cols[as.character(punq[,lab])]pu
+punq$cols <- cols[as.character(punq[,lab])]
 punq$pchs <- 21
 punq[lab=='RCT-gs-rp-cvd',pchs:=22]
 punq[lab=='RCT-gs-rp-maxRR',pchs:=24]
 ## Fig 4
 labstoshow <- c('RCT' ,'RCT-gs', 'RCT-rp', 'RCT-gs-rp', 'SWCT') 
 labstoshow <- punq[,unique(lab)]
-plunq <- punq[threshold==.05 & lab %in% labstoshow, list(lab, power, trialStartDate, threshold, above, above_EV, caseSpent, totCase, totCase_EV,avHaz, tcal,date, cols, pchs)]
+plunq <- punq[threshold==.05 & lab %in% labstoshow, list(lab, power, trialStartDate, threshold, above, above_EV, caseSpent_EV, totCase, totCase_EV,avHaz, tcal,date, cols, pchs)]
 
 ## ## ethicline contours
 ## ias <- pretty(plunq$caseSpent, n = 50)
@@ -70,10 +69,10 @@ for(dd in tmpM[,unique(date)]) {
     tmp <- tmpM[date==dd]
     xmax <- 60
     ## power
-    with(tmp, plot(caseSpent, power, xlim = c(0,xmax), ylim = c(0,1), pch = pchs, las = 1, col = cols, cex = 2, bty = 'n', xlab = ''))
+    with(tmp, plot(caseSpent_EV, power, xlim = c(0,xmax), ylim = c(0,1), pch = pchs, las = 1, col = cols, cex = 2, bty = 'n', xlab = ''))
     mtext('avertable cases not averted', 1, -1, outer = T)
     ## speed
-    with(tmp, plot(caseSpent, tcal/30, xlim = c(0,xmax), ylim = c(0,168/30), pch = pchs, las = 1, col = cols, cex=2, bty='n', xlab='', ylab = 'trial duration\n(months)'))
+    with(tmp, plot(caseSpent_EV, tcal/30, xlim = c(0,xmax), ylim = c(0,168/30), pch = pchs, las = 1, col = cols, cex=2, bty='n', xlab='', ylab = 'trial duration\n(months)'))
 }
     tmp[, legend('bottomright', leg = lab, col = cols, pch = pchs, cex = .7, bty = 'n')]
 graphics.off()
