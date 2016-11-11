0bab0866dcb0cbb8dc7077c38d2e9e92b5f63be3
Steven Bellan <steve.bellan@gmail.com>
2016-11-10 22:32:30 -0600

    figured out that the arm n otation is messed up for maxthresh & vaccdelay which is messing up plotting results. need to go upstream

--- IndivHet-Fig5.R
+++ IndivHet-Fig5.R
@@ -41,13 +41,13 @@ plunq <- punq[threshold==.05, list(lab, power, trialStartDate, threshold, above,
 ## playing with shit
 load('~/Documents/R Repos/EbolaVaccSim/BigResults/testRes.Rdata')
 names(resO)
+resList <- resO
 resList <- procIrskSpent(resList, verb=0)
 names(resList)
 irsk <- resList$irsk
 irsk$tid <- 1
 
 ## Create plot that shows risk averted
-
 
 ####################################################################################################
 ## infection risk
@@ -81,6 +81,52 @@ las = 1, xaxt='n', main =ll)]
 title(xlab='individual',outer=T)
 title(ylab='risk',outer=T)
 ##graphics.off()
+
+## so the problems are that 1) it's unlikely i'll be able to see the details for 6000, & 2) for all the trials. If I just show the average by risk group within each cluster, that shows complete info except doesn't highlight the breakdown by risk group.
+
+## the goal is to show how each design affects risk spent by specific groups differently
+
+names(itmp)
+itmp[lab=='RCT-gs-rp-cvd' & armO=='cont', unique(cluster)]
+itmp[lab=='RCT-gs-rp-cvd' & cluster==2, unique(arm)]
+Spop <- resList$Spop
+Spop[lab=='RCT-gs-rp-cvd' & arm=='contVD', unique(cluster)]
+Spop[lab=='RCT-gs-rp-cvd' & simNum==1 & sim==1 & indiv %% 150 %in% c(1,0), .(lab, Oi, Oc, indiv, cluster, arm)][order(indiv)]
+## for some reason only first cluster has a group labeled as contVD for these trials
+
+
+mtmp <- irsk[type=='cond', .(inf=mean(inf), spent=mean(spent), spent_EV=mean(spent_EV), avert_EV=mean(avert_EV), avert=mean(avert), n = length(avert_EV), Oi=Oi, Oc=Oc, arm=arm), .(cluster, armO, indivRR, lab)][order(cluster, armO, -indivRR)]
+mtmp[,ord:=order(Oc, indivRR, Oi)]
+mtmp[, cols:=armO]; mtmp[grepl('cont',cols),cols:='red']; mtmp[grepl('vacc',cols),cols:='dodger blue']
+mtmp[grepl('Excl',arm)]
+## also not seeing the averted risk amongst the maxRR plot
+
+
+par(mfrow=c(6,1), mar = c(0,3,1,0), oma = c(1,1,0,0))
+for(ll in mtmp[,unique(lab)]) {
+    mtmp[lab==ll, plot(ord, spent_EV, xlab='individual', ylab='risk spent', bty = 'n', type = 'h', col = cols, 
+             las = 1, xaxt='n', main =ll)]
+    with(mtmp[lab==ll], points(ord, -avert_EV, type = 'h', col = makeTransparent(cols, alpha = 250)))
+    abline(h=0, lty = 1)
+}
+##graphics.off()
+
+## look at all yvars for each trial type to see if things look right
+pdf(file.path(figdir, paste0('irsk InfSpentAvert by trial.pdf')), w = wid, h = heig)
+for(ll in mtmp[,unique(lab)]) {
+    par(mfrow=c(6,1), mar = c(0,3,1,0), oma = c(1,1,0,0))
+    for(yvar in c('inf', 'avert','avert_EV', 'spent', 'spent_EV')) {
+        ymax <- ifelse(yvar=='inf', 1, .15)
+        mtmp[lab==ll, plot(ord, get(yvar), xlab='individual', ylab='risk spent', bty = 'n', type = 'h', col = cols, 
+                 ylim = c(0, ymax)
+                 , las = 1, xaxt='n', main =yvar)]
+    abline(h=0, lty = 1)
+        mtext(ll, outer = T, side = 1, line = -1)
+    }
+}
+graphics.off()
+
+
 
 with(itmp[lab==ll], plot(ordShow, spent_EV, xlab='individual', ylab='risk spent', bty = 'n', type = 'h', col = cols, ylim = ylim, las = 1, xaxt='n', main =ll, xlim = c(0,600)))
 
